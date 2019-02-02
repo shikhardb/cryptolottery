4777d01cee5d5572054627593ba5852a3eaae2c2
Shikhar Dubey <shikhar.db@gmail.com>
2019-02-03 04:15:52 +0530

    Deploy contract using Infura

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'exchange lava way buffalo cheap vast reunion box keep fetch educate route',
+  'https://rinkeby.infura.io/v3/bc13752f4b8d4d38a16e8938390fbf5b'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to : ', result.options.address);
+};
+deploy();
