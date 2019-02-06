f9ed2643e0873832970c8dfca944276e8767286e
Shikhar Dubey <shikhar.db@gmail.com>
2019-02-07 04:26:24 +0530

    Pick a winner

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -38,6 +38,19 @@ class App extends Component {
     this.setState({ message: 'You have been entered' });
   };
 
+  onClick = async => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success'});
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({message: 'A winner has been picked!'});
+
+  }
+
   render() {
     return (
       <div>
@@ -62,6 +75,11 @@ class App extends Component {
           </div>
           <button>Enter</button>
         </form>
+
+        <hr />
+
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
 
         <hr />
 
