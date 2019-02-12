8dfc75c15baae2bb6c2c4dbf7cf91e3fffce4d29
Shikhar Dubey <shikhar.db@gmail.com>
2019-02-12 18:06:38 +0530

    Heroku postbuild

---
+++ out/_next/b334df57-2570-4edd-838a-ad200bf7cfa9/page/newCampaign/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/newCampaign', function() {
+            var comp = module.exports=webpackJsonp([8],{897:function(e,t,n){e.exports=n(898)},898:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(0),o=function(e){return e&&e.__esModule?e:{default:e}}(u);t.default=function(){return o.default.createElement("h1",null,"This is the new campaign page")}}},[897]);
+            return { page: comp.default }
+          })
+        
