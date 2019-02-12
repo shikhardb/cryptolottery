67ee93910918a61009ee5de41b1f103478a4b886
Shikhar Dubey <shikhar.db@gmail.com>
2019-02-12 15:34:39 +0530

    Row status styling

--- kickstarter-crypto/components/RequestRow.js
+++ kickstarter-crypto/components/RequestRow.js
@@ -6,8 +6,18 @@ import Campaign from '../ethereum/campaign';
 export default class RequestRow extends Component {
   onApprove = async () => {
     const campaign = Campaign(this.props.address);
+
     const accounts = await web3.eth.getAccounts();
     await campaign.methods.approveRequest(this.props.id).send({
+      from: accounts[0],
+    });
+  };
+
+  onFinalize = async () => {
+    const campaign = Campaign(this.props.address);
+
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.finalizeRequest(this.props.id).send({
       from: accounts[0],
     });
   };
@@ -15,9 +25,13 @@ export default class RequestRow extends Component {
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount } = this.props;
+    const readyToFinalize = request.approvalCount > approversCount / 2;
 
     return (
-      <Row>
+      <Row
+        disabled={request.complete}
+        positive={readyToFinalize && !request.complete}
+      >
         <Cell>{id}</Cell>
         <Cell>{request.description}</Cell>
         <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
@@ -26,9 +40,19 @@ export default class RequestRow extends Component {
           {request.approvalCount}/{request.approversCount}
         </Cell>
         <Cell>
-          <Button color="green" basic onClick={this.onApprove}>
-            Approve
-          </Button>
+          {request.complete ? null : (
+            <Button color="green" basic onClick={this.onApprove}>
+              Approve
+            </Button>
+          )}
+        </Cell>
+
+        <Cell>
+          {request.complete ? null : (
+            <Button color="teal" basic onClick={this.onFinalize}>
+              Finalize
+            </Button>
+          )}
         </Cell>
       </Row>
     );
