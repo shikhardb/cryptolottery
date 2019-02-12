fa2c4f66ea7ec3115d46d05d65b0fe926af3c9bf
Shikhar Dubey <shikhar.db@gmail.com>
2019-02-12 13:28:45 +0530

    Loading and error on campaign page

--- kickstarter-crypto/components/Contributeform.js
+++ kickstarter-crypto/components/Contributeform.js
@@ -7,12 +7,16 @@ import { Router } from '../routes';
 export default class Contributeform extends Component {
   state = {
     value: '',
+    errorMessage: '',
+    loading: false,
   };
 
   onSubmit = async event => {
     event.preventDefault();
 
     const campaign = Campaign(this.props.address);
+
+    this.setState({ loading: true, errorMessage: '' });
 
     try {
       const accounts = await web3.eth.getAccounts();
@@ -22,12 +26,16 @@ export default class Contributeform extends Component {
       });
 
       Router.replaceRoute(`/campaigns/${this.props.address}`);
-    } catch (err) {}
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false, value: '' });
   };
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to contribute</label>
           <Input
@@ -37,7 +45,10 @@ export default class Contributeform extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>Contribute</Button>
+        <Message error header="Oops!" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
+          Contribute
+        </Button>
       </Form>
     );
   }
