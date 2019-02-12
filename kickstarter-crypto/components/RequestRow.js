90a110cd40b2188b93311e73584fc88a667ec4c0
Shikhar Dubey <shikhar.db@gmail.com>
2019-02-12 15:17:29 +0530

    Approve a request

--- kickstarter-crypto/components/RequestRow.js
+++ kickstarter-crypto/components/RequestRow.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
+import Campaign from '../ethereum/campaign';
 
 export default class RequestRow extends Component {
+  onApprove = async () => {
+    const campaign = Campaign(this.props.address);
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.approveRequest(this.props.id).send({
+      from: accounts[0],
+    });
+  };
+
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount } = this.props;
@@ -15,6 +24,11 @@ export default class RequestRow extends Component {
         <Cell>{request.recipient}</Cell>
         <Cell>
           {request.approvalCount}/{request.approversCount}
+        </Cell>
+        <Cell>
+          <Button color="green" basic onClick={this.onApprove}>
+            Approve
+          </Button>
         </Cell>
       </Row>
     );
